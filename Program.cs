380dc385b72bbf90a35847f744db9f0a2ad72e8b
wuyuansushen <wuyuansushen@gmail.com>
2021-02-20 23:42:07 +0800

    Initial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeLambda
+{
+    class Program
+    {
+        static void DirectLambdaExpression()
+        {
+            Expression<Func<int, int>> lambdaMultiplication = x =>  x * x ;
+            Console.WriteLine(lambdaMultiplication);
+        }
+
+        static void APIExpression()
+        {
+            ParameterExpression parameter = Expression.Parameter(typeof(int), "x");
+            BinaryExpression binaryExpression = Expression.Multiply(parameter, parameter);
+            Expression<Func<int, int>> lambda1 = Expression.Lambda<Func<int, int>>(binaryExpression,new ParameterExpression[] {parameter});
+            Console.WriteLine(lambda1);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DirectLambdaExpression:");
+            DirectLambdaExpression();
+            Console.WriteLine("APIExpression:");
+            APIExpression();
+        }
+    }
+}
